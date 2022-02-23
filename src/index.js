ee87ed40fce245cd8fd9a254f2f9ed19ede1ceb0
Damira <damira.cyprus@gmail.com>
2022-02-23 21:09:40 +0300

    feat: add js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+  const ones = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  let numberStr = number.toString();
+  if (number === 0) return 'zero';
+  if (number < 20) {
+    return ones[number];
+  }
+  if (numberStr.length === 2) {
+    if (!ones[numberStr[1]]) {
+      return tens[numberStr[0]];
+    }
+    return tens[numberStr[0]] + ' ' + ones[numberStr[1]];
+  }
+  if (numberStr.length === 3) {
+    if (numberStr[1] === '0' && numberStr[2] === '0')
+      return ones[numberStr[0]] + ' hundred';
+    else
+      return (
+        ones[numberStr[0]] +
+        ' hundred ' +
+        toReadable(+(numberStr[1] + numberStr[2]))
+      );
+  }
+  if (numString.length === 4) {
+    const end = +(numberStr[1] + numberStr[2] + numberStr[3]);
+    if (end === 0) return ones[numberStr[0]] + ' thousand';
+    if (end < 100) return ones[numberStr[0]] + ' thousand' + toReadable(end);
+    return ones[numberStr[0]] + ' thousand ' + toReadable(end);
+  }
+};
